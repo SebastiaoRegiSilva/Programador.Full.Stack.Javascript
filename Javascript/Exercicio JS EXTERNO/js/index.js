d5d7317f31cc08d6e47874cd2197c5c2e4ceb8b2
Sebastião Willian <swillian2009@gmail.com>
2024-04-22 22:58:29 +0100

    EXERCICIO  - CONDICIONAIS

--- Javascript/Exercicio JS EXTERNO/js/index.js
+++ Javascript/Exercicio JS EXTERNO/js/index.js
@@ -17,20 +17,3 @@ alert(
 console.log(nomePrimeiraPessoa + " possui " + idadePrimeiraPessoa + " anos! \n");
 console.log(nomeSegundaPessoa + " possui " + idadeSegundaPessoa + " anos! \n")
 console.log("A diferença de idade entre " + nomePrimeiraPessoa + " e " + nomeSegundaPessoa, "são de " + diferencaIdade + " anos! \n");
-
-
-
-
-/* 1 Pedir o nome pessoa mais velha;
-2 Pedir o idade da pessoa mais velha; <br>
-
-    3 Pedir o nome da pessoa mais nova;
-    4 Pedir o idade da pessoa mais nova;<br>
-
-        5 Calcular a diferença de idade entre elas.<br>
-
-            6 Exibir o nome e idade da pessoa mais velha.<br>
-
-                7 Exibir o nome e idade da pessoa mais nova.<br>
-
-                    8 Exibir diferença de idade.<br></br> */
