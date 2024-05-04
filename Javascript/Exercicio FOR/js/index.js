70bb37c126ea902d582f49a2f33b975526f6fe6a
Sebastião Willian <swillian2009@gmail.com>
2024-05-04 10:10:10 +0100

    style: Adicionando alguns comentário úteis.

--- Javascript/Exercicio FOR/js/index.js
+++ Javascript/Exercicio FOR/js/index.js
@@ -1,16 +1,19 @@
+// Solicitar o nome da espacionave.
 let spaceship = prompt("Digite o nome da nave")
 
 let charToReplace = prompt("Qual caracter você deseja substituir?")
 
 let replacementChar = prompt("Por qual caracter você deseja substituir?")
 
+// Receber nome da nave caracter a caracter.
 let newSpaceship = ""
 
+// Elemental - substituir e por k
 for(let pos = 0; pos < spaceship.length; pos++) {
     if(spaceship[pos] == charToReplace) {
-        newSpaceship += replacementChar
+        newSpaceship += replacementChar // Elkmkntal
     } else {
-        newSpaceship += spaceship[pos]
+        newSpaceship += spaceship[pos] // Elemental
     }
 }
 
